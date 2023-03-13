77502eb5f8a454d4f1c920e728246cec6bbe0fa0
Luis Leal <luisleal98e@gmail.com>
2023-03-13 15:29:19 -0400

    Arreglo a consulta

--- src/controllers/clients.controller.ts
+++ src/controllers/clients.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express'
 import db from '../models'
 
-const clientsModel = db.clients
+const clientsModel = db.clients 
 
 export const clientList = async (req: Request, res: Response) => {
   try {
